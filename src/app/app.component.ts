be2b809195c88b1fe3bda4bc50503239538c92f5
Luiz Junior <jr.luizandrade@gmail.com>
2022-10-03 10:11:27 -0300

    Criando uma pagina de profile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,6 @@ import { Nav, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-
-
-
 @Component({
   templateUrl: 'app.html'
 })
@@ -21,9 +18,9 @@ export class MyApp {
 
     // used for an example of ngFor and navigation
     this.pages = [
-      { title: 'Home', component: 'HomePage' }
+      { title: 'Profile', component: 'ProfilePage' },
+      { title: 'Categorias', component: 'CategoriasPage' }
     ];
-
   }
 
   initializeApp() {
