37614c781a8ff136fcc4d493e91ac5bbb9fe13bd
Luiz Junior <jr.luizandrade@gmail.com>
2022-11-02 09:53:21 -0300

    Criando FormGroup para controlar formulario

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 
@@ -10,7 +11,31 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 export class SignupPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  formGroup: FormGroup;
+
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    public formBuilder: FormBuilder) {
+
+    //instaciando o formgroup
+    this.formGroup = this.formBuilder.group({
+      nome: ['Luiz', [Validators.required, Validators.minLength(5), Validators.maxLength(120)]],
+      email: ['luizjr@email.com', [Validators.required, Validators.email]],
+      tipo: ['1', [Validators.required]],
+      cpfOuCnpj: ['61870765168', [Validators.required, Validators.minLength(11), Validators.maxLength(14)]],
+      senha: ['123', [Validators.required]],
+      logradouro: ['rua 2', [Validators.required]],
+      numero: ['12', [Validators.required]],
+      complemento: ['casa 31', [Validators.required]],
+      bairro: ['Pds', []],
+      cep: ['77022400', [Validators.required]],
+      telefone1: ['6322225522', [Validators.required]],
+      telefone2: ['', []],
+      telefone3: ['', []],
+      estadoId: [null, [Validators.required]],
+      cidadeId: [null, [Validators.required]],
+    });
   }
 
   signupUser(){
